34d777c5229665ce94a1fb9b00baa680c6c74480
DevRabiul <devrabiul@gmail.com>
2025-01-04 12:36:28 +0600

    Update

--- resources/views/frontend/layouts/partials/_style-assets.blade.php
+++ resources/views/frontend/layouts/partials/_style-assets.blade.php
@@ -1,3 +1,18 @@
+<meta name="description" content="Effortlessly manage courses with our Online Course Management System. Features include scheduling, tracking, assessments, and interactive tools for seamless learning.">
+<meta name="robots" content="index, follow">
+
+<meta property="og:type" content="website">
+<meta property="og:title" content="Online Course Management System">
+<meta property="og:url" content="{{ route('frontend.index') }}">
+<meta property="og:image" content="{{ getDynamicAsset('assets/img/meta-thumbnail.png') }}">
+<meta property="og:description" content="Effortlessly manage courses with our Online Course Management System. Features include scheduling, tracking, assessments, and interactive tools for seamless learning.">
+<meta itemprop="image" content="{{ getDynamicAsset('assets/img/meta-thumbnail.png') }}">
+
+<meta name="twitter:card" content="summary">
+<meta name="twitter:title" content="Online Course Management System">
+<meta name="twitter:image" content="{{ getDynamicAsset('assets/img/meta-thumbnail.png') }}">
+<meta name="twitter:description" content="Effortlessly manage courses with our Online Course Management System. Features include scheduling, tracking, assessments, and interactive tools for seamless learning.">
+
 <link rel="stylesheet" async href="{{ getDynamicAsset('assets/libs/bootstrap-5.3.3/css/bootstrap.min.css') }}">
 <link rel="stylesheet" async href="{{ getDynamicAsset('assets/libs/bootstrap-5.3.3/css/bootstrap.custom.css') }}">
 <link rel="stylesheet" async href="{{ getDynamicAsset('assets/libs/bootstrap-icons-1.11.3/bootstrap-icons.min.css') }}">
